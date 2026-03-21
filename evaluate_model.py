2e2b122e7dc91142ce6eeec76ae3bb43af140193
Nick-2002b <nicolae.balaban@studio.unibo.it>
2026-03-21 17:52:17 +0100

    feat: setup test dataset and dataloader for evaluation

--- evaluate_model.py
+++ evaluate_model.py
@@ -14,6 +14,7 @@ def evaluate_baseline():
     BASE_DIR = Path(__file__).resolve().parent
     DATA_ROOT = BASE_DIR / "data"
     MODEL_PATH = BASE_DIR / "runs" / "base_autoencoder.pth"
+    CATEGORY = "bottle"
 
     device = torch.device(
         "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
@@ -21,3 +22,16 @@ def evaluate_baseline():
     model = SimpleAutoencoder().to(device)
     model.load_state_dict(torch.load(MODEL_PATH, map_location=device, weights_only=True))
     model.eval()
+
+    transform_pipeline = T.Compose([
+        T.Resize((256, 256)),
+        T.ToTensor()
+    ])
+
+    test_dataset = MVTecDataset(DATA_ROOT, CATEGORY, is_train=False, transform=transform_pipeline)
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+    cp.yellow(f"Evaluating of {len(test_dataset)} test images...")
+
+    y_true = []
+    y_scores = []
