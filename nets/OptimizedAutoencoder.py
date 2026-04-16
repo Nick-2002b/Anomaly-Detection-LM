c2c9b9f53198f429e0cd932ba415c656b3819cf0
Nick-2002b <nicolae.balaban@studio.unibo.it>
2026-04-16 19:41:23 +0200

    feat(nets): implement OptimizedAutoencoder net

---
+++ nets/OptimizedAutoencoder.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+
+from MVTec_Anomaly_Detection import reconstructed
+
+
+class OptimizedAutoencoder(nn.Module):
+    def __init__(self):
+        super(OptimizedAutoencoder, self).__init__()
+        self.encoder = None
+        self.decoder = None
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2),
+
+            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(0.2),
+
+            nn.ConvTranspose2d(in_channels=32, out_channels=3, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        latent = self.decoder(x)
+        reconstructed = self.decoder(latent)
+        return reconstructed
